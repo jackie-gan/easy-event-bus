5457c0371eed6f7316f1fa766989941f2960efc0
jackie-gan <jackiegan405679096@gmail.com>
2019-09-05 23:09:32 +0800

    feat: add event bus

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
 class EventBus {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event: string, callback) {
+    if (typeof event !== 'string') {
+      console.error('event should be string');
+      return;
+    }
+
+    this.events[event].push(callback);
+
+    return this;
+  }
+
+  trigger(event: string, data: Object) {
+    if (typeof event !== 'string') {
+      console.error('event should be string');
+      return;
+    }
+
+    const callbacks = this.events[event] || [];
+
+    for (let i = 0, len = callbacks.length; i < len; i++) {
+      callbacks[i](data);
+    }
+
+    return this;
+  }
 }
